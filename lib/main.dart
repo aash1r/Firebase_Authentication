315f458d3e4d8621a527905069539493ed04aebb
aash1r <aashirisani@gmail.com>
2023-10-03 02:06:57 +0500

    cloud firestore implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,13 +9,14 @@ import 'package:flutter_firebase/screens/phone_auth/signin_with_phone.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your appl ication.
+  // This widget is the   root of your appl ication.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase/screens/email_auth/login_screen.dart';
 import 'package:flutter_firebase/screens/phone_auth/signin_with_phone.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +11,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController namecontroller = TextEditingController();
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void saveUser() async {
+    String name = namecontroller.text.trim();
+    String email = emailcontroller.text.trim();
+
+    namecontroller.clear();
+    emailcontroller.clear();
+
+    if (name != "" && email != "") {
+      await firestore.collection("Users").add({"name": name, "email": email});
+    } else {
+      print("Please fill all the fields");
+    }
+  }
+
   void logout() async {
     await FirebaseAuth.instance.signOut();
   }
@@ -34,6 +53,54 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               icon: const Icon(Icons.logout_outlined))
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            TextField(
+              controller: namecontroller,
+              decoration: const InputDecoration(hintText: "Name"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: emailcontroller,
+              decoration: const InputDecoration(hintText: "Email address"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              color: Colors.teal,
+              onPressed: () {
+                saveUser();
+              },
+              child: const Text(
+                "Save",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            StreamBuilder(
+                stream: firestore.collection("Users").snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    if (snapshot.hasData && snapshot.data != null) {
+                      ListView.builder(itemBuilder: (context, index) {});
+                    }
+                  } else {
+                    Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListTile();
+                })
+          ],
+        ),
       ),
     );
   }
